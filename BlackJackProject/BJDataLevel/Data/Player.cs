cc064dd48d29042b66b5d74e454f4249f8e0194a
whitewAw <whitewAw@gmail.com>
2017-04-19 20:56:54 +0300

    createBase

---
+++ BlackJackProject/BJDataLevel/Data/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJDataLevel.Data
+{
+    public class Player:BaseEntity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Balance { get; set; }
+        public int CreditBalance { get; set; }
+    }
+}
