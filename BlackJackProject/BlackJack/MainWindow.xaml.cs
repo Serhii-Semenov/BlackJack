abe39e01a36bc8e89c9b512f5934d9cf5b117223
Serhii <semenov@mksat.net>
2017-04-20 19:13:23 +0300

    Add window Menu

--- BlackJackProject/BlackJack/MainWindow.xaml.cs
+++ BlackJackProject/BlackJack/MainWindow.xaml.cs
@@ -20,63 +20,20 @@ namespace BlackJack
     /// </summary>
     public partial class MainWindow : Window
     {
-        StackPanel ButtonsStackPanel;
-        Button LoginButton;
-        Button RegisterButton;
-        Image BlackJackImage;
-        TabControl LoginTabControl;
-        TabControl RegisterTabControl;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            CreateControls();
         }
-
-        void CreateControls()
-        {
-            ButtonsStackPanel = new StackPanel();
-            ButtonsStackPanel.Height = 45;
-            ButtonsStackPanel.Width = 220;
-            ButtonsStackPanel.Orientation = Orientation.Horizontal;
-            ButtonsStackPanel.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
-            ButtonsStackPanel.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-
-                        
-            //создание кнопок
-            LoginButton = new Button();
-            RegisterButton = new Button();
-            //текст кнопок
-            LoginButton.Content = "Авторизация";
-            RegisterButton.Content = "Регистрация";
-            //курсор
-            LoginButton.Cursor = Cursors.Hand;
-            RegisterButton.Cursor = Cursors.Hand;
-            //кнопки по центру стекпанели
-            LoginButton.VerticalAlignment = System.Windows.VerticalAlignment.Center;
-            RegisterButton.VerticalAlignment = System.Windows.VerticalAlignment.Center;
-
-
-            //кнопки в стекпанель
-            ButtonsStackPanel.Children.Add(LoginButton);
-            ButtonsStackPanel.Children.Add(RegisterButton);
-
-
-            //картинка
-            BlackJackImage = new Image();
-            BlackJackImage.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
-            BlackJackImage.VerticalAlignment = System.Windows.VerticalAlignment.Center;
-            
-
-
-            MainWindowGrid.Children.Add(ButtonsStackPanel);
-        }
-
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btnMenuWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new MenuWindow();
+            w.ShowDialog();
         }
     }
 }
