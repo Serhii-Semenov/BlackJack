99009cf1ed5cc2472971003c71dfb68c8a107678
whitewAw <whitewAw@gmail.com>
2017-04-20 17:36:36 +0300

    Попытка создание БД

--- BlackJackProject/BJDataLevel/Data/Player.cs
+++ BlackJackProject/BJDataLevel/Data/Player.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BJDataLevel.Data
+﻿namespace BJDataLevel.Data
 {
     public class Player:BaseEntity
     {
