617705cefc21ae5937b5b471f24bf20bc61aa0a3
whitewAw <whitewAw@gmail.com>
2017-04-26 20:55:46 +0300

    word db

---
+++ BlackJackProject/BJDataLevel/Model/Players.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BJDataLevel.Model
+{
+    public class Players
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Players()
+        {
+            BalansPlayers = new HashSet<BalansPlayers>();
+            Statistics = new HashSet<Statistics>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BalansPlayers> BalansPlayers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Statistics> Statistics { get; set; }
+
+    }
+}
